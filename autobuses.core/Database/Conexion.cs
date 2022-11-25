ab6052e9f5f3e75b55ec25d72aa417b4d32232f0
A20021 <aldrich.iberri228878@potros.itson.edu.mx>
2022-11-24 21:51:33 -0700

    Add project files.

---
+++ autobuses.core/Database/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autobuses.core.Database
+{
+    public class Conexion{
+
+        public MySqlConnection Connection;
+
+        public Conexion(){
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+            Connection = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection(){
+            try{
+                Connection.Open();
+                return true;
+            }
+            catch (MySqlException ex){
+                throw ex;
+            }
+        }
+
+        public bool CloseConnection(){
+            try{
+                Connection.Close();
+                return true;
+            }
+            catch (MySqlException ex){
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ autobuses/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="autobuses.MvcApplication" Language="C#" %>
